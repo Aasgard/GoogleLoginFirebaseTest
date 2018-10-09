549fd13311f0bf2931882238d977bf71d39b5422
François-Régis LANCIEN <francois.regis.lancien@gmail.com>
2018-10-09 14:52:36 +0200

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,59 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {GooglePlus} from "@ionic-native/google-plus";
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAsenoQ8Vz2JjQzeIZHESeD3yvk4wQBKnA",
+        authDomain: "testlogin-68cac.firebaseapp.com",
+        databaseURL: "https://testlogin-68cac.firebaseio.com",
+        projectId: "testlogin-68cac",
+        storageBucket: "testlogin-68cac.appspot.com",
+        messagingSenderId: "826626010601"
+    }
+};
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage
+    ],
+    providers: [
+        GooglePlus,
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,88 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {LoadingController, Platform} from 'ionic-angular';
+import {AngularFireAuth} from "@angular/fire/auth";
+import {GooglePlus} from "@ionic-native/google-plus";
+import * as firebase from "firebase";
+import {User} from "firebase";
+import {from} from "rxjs/observable/from";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    user: User;
 
-  }
+    constructor(private loadingCtrl: LoadingController,
+                private afAuth: AngularFireAuth,
+                private gplus: GooglePlus,
+                private platform: Platform) {
+        this.afAuth.user.subscribe(user => {
+            this.user = user;
+        });
+    }
+
+    private nativeGoogleLogin(): void {
+        const loader = this.loadingCtrl.create();
+
+        if (!this.user) {
+            loader.present();
+            this.gplus.login({
+                'webClientId': '826626010601-fcjt337ot5qahroestmkdnisn53tlm3u5.apps.googleusercontent.com'
+            }).then(user => {
+                from(this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(user.idToken))).fina.then(userConnected => {
+                    if (userConnected) {
+                        this.user = userConnected;
+                    }
+                }).catch(error => {
+                    alert(JSON.stringify(error));
+                })
+            }).catch(err => {
+                alert(JSON.stringify(err));
+            })
+        }
+
+        // try {
+        //
+        //     if (!this.user) {
+        //         const gplusUser = await this.gplus.login({
+        //             'webClientId': '826626010601-fcjt337ot5qahroetmkdnisn53tlm3u5.apps.googleusercontent.com'
+        //         });
+        //         loader.dismiss();
+        //         return await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken))
+        //     }
+        //
+        // } catch (err) {
+        //     console.log(err)
+        // }
+    }
+
+    async webGoogleLogin(): Promise<void> {
+        try {
+            const credential = await this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(user => {
+                if (user) {
+                    alert(JSON.stringify(user.user));
+                    this.user = user.user;
+                }
+            });
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    googleLogin() {
+        if (this.platform.is('cordova')) {
+            this.nativeGoogleLogin();
+        } else {
+            this.webGoogleLogin();
+        }
+    }
+
+    signOut() {
+        this.afAuth.auth.signOut();
+    }
 
 }
